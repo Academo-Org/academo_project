6a9e7bad788e3502d572d0a1298d63cfa5c29d54
Sofia <carina.b.sofia@gmail.com>
2025-10-01 08:34:01 -0300

    Site academo

---
+++ script.js
@@ -0,0 +1,17 @@
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+const carouselImages = document.querySelector(".carousel-images");
+const images = document.querySelectorAll(".carousel-images img");
+
+let index = 0;
+
+function showSlide(i) {
+  if (i < 0) index = images.length - 1;
+  else if (i >= images.length) index = 0;
+  else index = i;
+
+  carouselImages.style.transform = `translateX(${-index * 100}%)`;
+}
+
+prev.addEventListener("click", () => showSlide(index - 1));
+next.addEventListener("click", () => showSlide(index + 1));
